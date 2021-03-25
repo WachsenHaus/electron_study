66c3480497575d27db4d54950bcaa8467fcfc3f5
Wachsenhaus <bi9choi@gmail.com>
2021-03-25 11:57:31 +0900

    <추가> : <자식요소를 넣는 방법 jsx.element>

--- hello/src/wrapper.tsx
+++ hello/src/wrapper.tsx
@@ -1,10 +1,12 @@
 import React from "react";
+
+//children에 jsx를 해주어야 하위 자식들이 렌더링이 잘된다. 
 type Props = {
     children: JSX.Element|JSX.Element[];
   };
  const Wrapper = ({children}:Props) => {
     const style = {
-        border: `2px solid black`,
+        border: `2px solid red`,
         padding : `16px`
     }
     return(
